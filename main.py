3a47780e22d7d6f54d37ee1b3aa37ab8cdf52042
kellydepuy <kellydepuy@gmail.com>
2022-07-02 00:00:29 -0400

    Can now remove items from either list item ids will now reset at 1000

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ garbage_bin = {}
 
 
 print("Welcome to Brain Dump! You've got enough on your mind. Why don't you leave some stuff here? I'll remember it for you and you can retrieve it later.")
+print("Once your to do list or thought garbage bin reaches 1000, the first items you added will be overwritten by new entries.")
 exit = False
 num = 1
-key = 2
+key = 1
 while exit == False:
+    if num > 1000:
+        num = 1
+    if key > 1000:
+        key = 1
     choice = input("Type 1 to access the to do list. Type 2 to store a thought you want to throw away.")
     if choice == "1":
         input1 = input("Type 1 to read your to do list. Type 2 to add to your to do list. Type 3 to remove an item from you to do list")
@@ -14,30 +19,28 @@ while exit == False:
             print(to_do_list_dict)
         elif input1 == "2":
             input2 = input("What to you need to add?")
-            to_do_list_dict[num] = input2
+            to_do_list_dict[str(num)] = input2
             num += 1
         elif input1 == "3":
             print(to_do_list_dict)
             remove_item = input("Which number needs to be removed?")
-            del remove_item
+            del to_do_list_dict[remove_item]
         elif input1 == "exit":
             exit = True
         else:
             print("Sorry. That wasn't one of the options. Try again.")
-            to_do_list()
     elif choice == "2":
-        new_choice = input(
-            "Type 1 to add a thought. Type 2 to review old thoughts. Not recommended. You left them here for a reason. Type 3 if you'd like to remove a thought.")
+        new_choice = input("Type 1 to add a thought. Type 2 to review old thoughts. Not recommended. You left them here for a reason. Type 3 if you'd like to remove a thought.")
         if new_choice == "1":
             thought = input("What's on your mind?")
-            garbage_bin[key] = thought
+            garbage_bin[str(key)] = thought
             key += 1
         elif new_choice == "2":
             print(garbage_bin)
         elif new_choice == "3":
             print(garbage_bin)
             removed_item = input("Which number would you like to remove?")
-            del removed_item
+            del garbage_bin[removed_item]
         elif new_choice == "exit":
             exit = True
         else:
